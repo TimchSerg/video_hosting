78785fd385819718b29665da4a00bb462435eb80
TimchSerg <Grimmmello@mail.ru>
2024-11-15 16:37:45 +0300

    (added): connect bd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,39 @@
 import { Module } from '@nestjs/common';
 import { VideoModule } from '../video/video.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { SequelizeModule, SequelizeModuleOptions } from '@nestjs/sequelize';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import configuration from 'src/config/configuration';
+
+async function sequelizeFactory (
+  configService: ConfigService
+): Promise<SequelizeModuleOptions> {
+
+  const config = configService.get<SequelizeModuleOptions>('database');
+
+  if (config === undefined) {
+    throw new Error('Database configuration was not found')
+  }
+
+  config.models = [
+    
+  ]
+console.log(config)
+  return config
+}
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+    }),
+    SequelizeModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: sequelizeFactory
+    }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '../../', 'client'),
       exclude: ['/api/(.*)'],
